0add485f19cf2289679088045153f970f1847aa6
Leandro <leandropacsi27@gmail.com>
2025-11-08 16:15:52 -0500

    build para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from "astro/config";
 export default defineConfig({
   site: "https://leandropacsi27.github.io/Portafolio-2025/",
   base: "/Portafolio-2025/",
-  outDir: "./dist", // carpeta de salida
+  // carpeta de salida
 });
